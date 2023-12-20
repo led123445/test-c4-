d7a060efd27b20aa76e0b5370d78ee64696059cf
Tran Anh Duc <tran27082003@gmail.com>
2023-12-20 15:23:28 +0700

    Create login.cs in folder controllers

---
+++ test c4/Controllers/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_c4.Controllers
+{
+    internal class Login
+    {
+    }
+}
